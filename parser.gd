87627fdfba1d7bb8d566c8d8a9a42e5ad5950639
Piyan0 <piyanapriyanto.0@gmail.com>
2026-05-19 13:26:42 +0700

    m

--- parser.gd
+++ parser.gd
@@ -1,47 +1,94 @@
 class_name PiyanScript
 extends RefCounted
 
+const DELIMITER = " "
+const QUOTE = "\""
+const OPERATOR_LIST = ["==", ">", ">=", "<", "<=", "!="]
+
+const KEYWORD_IF = "if"
+const KEYWORD_ENDIF = "endif"
+const KEYWORD_LOOP= "loop"
+const KEYWORD_BREAK= "break"
+const KEYWORD_ENDLOOP= "endloop"
+const KEYWORD_exit= "exit"
+
 var variable = {
     x = true,
-    y = "Hawa"
+    y = 1
 }
 
-const delimiter = " "
-const quote = "\""
-
-var built_in_code = {
-         
+var _block = {
+    KEYWORD_IF : KEYWORD_ENDIF,
+    KEYWORD_LOOP : KEYWORD_ENDLOOP,
 }
+
+var _custom_split = {
+    KEYWORD_IF : (func(line):
+        line = line.replace(KEYWORD_IF, "")
+        var op = ""
+        for operator in OPERATOR_LIST:
+            if operator in line:
+                op = operator
+                break
+        var condition = line.split(op)
+        
+        var result = [KEYWORD_IF, condition[0].strip_edges(), op, condition[1].strip_edges()]
+        return result
+        ),
+}
+
+var _built_in_keyword_runner = {
+    # remaining_lines add arg
+    KEYWORD_IF : func(args):
+        var _var = _get_var(args[0])
+        var op = args[1]
+        var expected_value = args[2]
+        
+        if _is_compare_pass(_var, str_to_var(expected_value), op):
+            var block = args.back()
+            for line in block:
+                await _run_line(line)
+                
+}
+
 
 func _init() -> void:
     var line = "text salwa \"nah ini dia yamg aku maksud bro..\" token_1"
     var line_2 = ("
-if $salwa == 2
+if $y==1
         text salwa
-    open-inv $my_var
-endif
-text piyan
-    "    
+        open-inv $my var
+        exit
+        
+        if $x == true
+            text this-is-nested-loop...
+        endif
+endif" 
     )
+    
     var lines = _split_lines(line_2)
-    _run_lines(lines)
-    # print(_tokenize_line(line_2))
+    print(lines)
+    #var x = _preparse(lines)
 
 
-func _tokenize_line(line):
+func _split_args(line):
     var args = []
     var word = ""
-    var inside_quote = false
-
+    var inside_QUOTE = false
+    
+    for custom_split_id in _custom_split:
+        if line.begins_with(custom_split_id):
+            return _custom_split[custom_split_id].call(line)
+    
     for char in line:
-        if char == delimiter:
-            if !inside_quote:
+        if char == DELIMITER:
+            if !inside_QUOTE:
                 args.append(word)
                 word = ""
                 continue
             
-        if char == quote:
-            inside_quote = !inside_quote
+        if char == QUOTE:
+            inside_QUOTE = !inside_QUOTE
             continue
         word += char
     
@@ -50,6 +97,25 @@ func _tokenize_line(line):
     
     return args
 
+
+func _preparse(lines):
+    var parsed_line = []
+    var current_block = []
+    while !lines.is_empty():
+        var line = lines.pop_front()
+        line = _split_args(line)
+        var code = line[0]
+        if code in _block:
+            var block_data = _parse_block(lines.duplicate(), _block[code])
+            lines = block_data.lines
+            line.push_back(block_data.block)
+            parsed_line.append(line)
+        else:
+            parsed_line.append(line)
+            
+    return parsed_line
+    
+    
 func _split_lines(lines_str):
     var lines = []
     var split = lines_str.split("\n")
@@ -63,24 +129,44 @@ func _split_lines(lines_str):
 
 
 func _find_pair_from_lines_arr(lines: Array, pair_end):
+    var line_count = 0
     var valid_lines = []
     for line in lines:
         if line == pair_end:
-            return valid_lines
+            return {lines = lines, line_count = line_count}
+        line_count += 1
         valid_lines.push_back(line)
+
+
+func _run_line(line):
+    var temp_line = line.duplicate()
+    var code = temp_line.pop_front()
+    var args = temp_line
+    printt("running", code)
+    if code in _built_in_keyword_runner:
+        await _built_in_keyword_runner[code].call(args)
+
+            
+func _parse_block(lines, start_pair, end_pair):
+    var block = []
     
-    return valid_lines
+func _get_var(var_str):
+    var var_id = var_str.replace("$", "")
+    assert(var_id in variable, str(var_id))
+    return variable[var_id]
 
 
-func _run_lines(lines):
-    print(lines)
-    return 
-    for line in lines:
-        var token = _tokenize_line(line)
-        var code = token.pop_front()
-
-        print(code)
-
-
-func _detect_builtin_keyword():
-    pass
+func _is_compare_pass(var_source, expected_str, operator):
+    match operator:
+        "==":
+            return var_source == expected_str
+        ">":
+            return var_source > expected_str
+        ">=":
+            return var_source >= expected_str
+        "<":
+            return var_source < expected_str
+        "<=":
+            return var_source <= expected_str
+        "!=":
+            return var_source != expected_str
