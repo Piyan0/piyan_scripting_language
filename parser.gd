e0740e5be2f0ef45a8e55543c14d1c3ccb579aa2
Piyan0 <piyanapriyanto.0@gmail.com>
2026-05-19 14:27:08 +0700

    m

--- parser.gd
+++ parser.gd
@@ -52,6 +52,12 @@ var _built_in_keyword_runner = {
 }
 
 
+var runner = {
+    text = func(args):
+        print("text with ", args[0])
+}
+
+
 func _init() -> void:
     var line = "text salwa \"nah ini dia yamg aku maksud bro..\" token_1"
     var line_2 = ("
@@ -63,13 +69,13 @@ if $y==1
         if $x == true
             text this-is-nested-loop...
         endif
-endif" 
+endif
+text anjay-mabar
+" 
     )
-    
     var lines = _split_lines(line_2)
-    print(lines)
-    #var x = _preparse(lines)
-
+    var x = _preparse(lines)
+    print(JSON.stringify(x, "\t"))
 
 func _split_args(line):
     var args = []
@@ -98,24 +104,23 @@ func _split_args(line):
     return args
 
 
-func _preparse(lines):
-    var parsed_line = []
-    var current_block = []
+func _preparse(lines: Array):
+    var parsed_lines = []
     while !lines.is_empty():
         var line = lines.pop_front()
-        line = _split_args(line)
-        var code = line[0]
-        if code in _block:
-            var block_data = _parse_block(lines.duplicate(), _block[code])
-            lines = block_data.lines
-            line.push_back(block_data.block)
-            parsed_line.append(line)
+        if line.begins_with(KEYWORD_IF):
+            var block_data = _parse_block(lines, KEYWORD_IF, KEYWORD_ENDIF)
+            for i in range(0, block_data.line_count):
+                lines.pop_front()
+
+            parsed_lines.push_back({source = line, block = _preparse(block_data.block)})
+
         else:
-            parsed_line.append(line)
-            
-    return parsed_line
+            parsed_lines.push_back({source = line})
     
+    return parsed_lines
     
+
 func _split_lines(lines_str):
     var lines = []
     var split = lines_str.split("\n")
@@ -139,16 +144,41 @@ func _find_pair_from_lines_arr(lines: Array, pair_end):
 
 
 func _run_line(line):
-    var temp_line = line.duplicate()
-    var code = temp_line.pop_front()
-    var args = temp_line
+    var code = line.pop_front()
+    var args = line
     printt("running", code)
     if code in _built_in_keyword_runner:
-        await _built_in_keyword_runner[code].call(args)
+        await _built_in_keyword_runner[code].callv(args)
+    else:
+        await runner[code].callv(args)
 
+
+func run_lines(sources):
+    pass
             
 func _parse_block(lines, start_pair, end_pair):
     var block = []
+    var pair_found = 1
+    var line_count = 0
+    for line in lines:
+        
+        block.append(line)
+        line_count += 1
+        if line.begins_with(start_pair):
+            pair_found += 1
+            # print("found", pair_found)
+            continue
+        elif line.begins_with(end_pair):
+            pair_found -= 1
+
+            if pair_found == 0:
+                # erase the end pair at the very back.
+                block.pop_back()
+                var result = {block = block, line_count = line_count}
+                return result
+            # print("end pair", pair_found)
+
+                
     
 func _get_var(var_str):
     var var_id = var_str.replace("$", "")
