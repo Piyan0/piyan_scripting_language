e728282a6d8f50c41b3dcd01950e78ea108d4831
Piyan0 <piyanapriyanto.0@gmail.com>
2026-05-20 20:14:32 +0700

    m

--- parser.gd
+++ parser.gd
@@ -1,5 +1,5 @@
 class_name PiyanScript
-extends RefCounted
+extends Node
 
 const DELIMITER = " "
 const QUOTE = "\""
@@ -21,73 +21,29 @@ const Keyword = {
 }
 
 var variable = {
-    x = true,
-    y = 2,
-    z = true
+    x = 1,
+    y = true,
 }
-
-
 var _labels = {}
-
-var _block = {
-    Keyword.IF : Keyword.ENDIF,
-    Keyword.LOOP : Keyword.ENDLOOP,
-}
-
-var _keyword_split = {
-    Keyword.IF : func(line: String):
-        line = line.substr(Keyword.IF.length() + DELIMITER.length())
-        var op = ""
-        for operator in OPERATOR_LIST:
-            if operator in line:
-                op = operator
-                break
-        
-        if op.is_empty():
-            var split = Array(line.split(DELIMITER))
-            var var_id = split.pop_front().strip_edges()
-            var func_name = var_id.replace("$", "")
-            if func_name in _custom_if_condition:
-                return [Keyword.IF, var_id, split]
-            else:
-                return [Keyword.IF, var_id, "==", "true"]
-
-        var condition = line.split(op)
-        var result = [Keyword.IF, condition[0].strip_edges(), op, condition[1].strip_edges()]
-        return result
-        ,
-}
-
-var _runner = {
-    Keyword.IF: _runner_if,
-    Keyword.LOOP : _runner_loop,
-    Keyword.JUMP : _runner_jump,
-    Keyword.SET : _runner_set,
-        
-    "text" : func(args, block, remaining_commands):
-        print("text with ", args[0])        
-        if remaining_commands.front() == null || remaining_commands.front() != "text":
-            print("showing dialogue...")
-        await Engine.get_main_loop().create_timer(1).timeout
-        ,
-
-    "shop" : func(args, block, remaining_commands):
-        print("shopping")        
-        await Engine.get_main_loop().create_timer(1).timeout
-        ,
-                
-}
+var _callbacks = {}
+var _setted_var = {}
 
 
-var _custom_if_condition = {
-    "test" = func(text):
-        if text == "anjay":
+func _init() -> void:
+    _callbacks.test = func(arg):
+        # print("args ", arg)
+        if arg == "godot":
             return true
-
         return false
-}
 
-var _setted_var = {"_internal_var" : "anjay"}
+    _callbacks.test2 = func(args, remaining_commands):
+        print("test2 with ", args[0])        
+        await Engine.get_main_loop().create_timer(1).timeout
+        
+    _setted_var.test = "_internal var"
+
+    # print(_split_args("anjay \"keren euy\""))
+    # print(_check_if_conditions("if $test oke"))
 
     
 func from_path(path):
@@ -99,6 +55,7 @@ func from_text(text):
     var labels = _parse_label(text)
     _labels = labels.labels
     var lines = labels.main_commands
+    # print(lines)
     await run(lines)
 
 
@@ -106,11 +63,7 @@ func _split_args(line):
     var args = []
     var word = ""
     var inside_QUOTE = false
-    
-    for custom_split_id in _keyword_split:
-        if line.begins_with(custom_split_id):
-            return _keyword_split[custom_split_id].call(line)
-    
+   
     for char in line:
         if char == DELIMITER:
             if !inside_QUOTE:
@@ -146,27 +99,27 @@ func _split_lines(lines_str):
     return lines
 
     
-    
 func _get_var(var_str):
     var var_id = var_str.replace("$", "")
     assert(var_id in variable, str(var_id))
     return variable[var_id]
 
 
-func _is_compare_pass(var_source, expected_str, operator):
+func _is_compare_pass(source_var, operator, expected_var):
+    # printt("source_var ", source_var, "operator ",operator, "expected_var ", expected_var)
     match operator:
         "==":
-            return var_source == expected_str
+            return source_var == expected_var
         ">":
-            return var_source > expected_str
+            return source_var > expected_var
         ">=":
-            return var_source >= expected_str
+            return source_var >= expected_var
         "<":
-            return var_source < expected_str
+            return source_var < expected_var
         "<=":
-            return var_source <= expected_str
+            return source_var <= expected_var
         "!=":
-            return var_source != expected_str
+            return source_var != expected_var
 
 
 func _parse_label(text):
@@ -196,21 +149,77 @@ func _parse_label(text):
     return {labels = labels, main_commands = main_commands}
 
 
-func _get_commands_from_sources(sources):
+func _get_commands_from_sources(lines):
     var commands = []
-    for source in sources:
-        var split = _split_args(source.source)
+    for line in lines:
+        var split = _split_args(line)
         commands.append(split[0])
     
     return commands
 
 
-func run(lines):
-    var line_index = -1
-    while true:
-        line_index += 1
-        if line_index > lines.size()-1:
+
+func _check_if_conditions(code = "if $y == on"):
+    # print("if ", code)
+    var regex = RegEx.new()
+    # if $(x) (==) (on)
+    regex.compile(r"if\s*\$(\w+)\s*(==|!=)\s*(\w+)")
+    var result= regex.search(code)
+
+    if result != null:
+        var match = result.strings
+        # print(match)
+        return _is_compare_pass(variable[match[1]], match[2], str_to_var(match[3]))
+    else:
+        # if $(x) (args)]
+        regex.compile(r"if \$(\w+)\s*(.*)")
+        result = regex.search(code)
+        var match = result.strings
+        var id = match[1]
+
+        if id in variable:
+            return _is_compare_pass(variable[match[1]], "==", true)
+        else:
+            assert(id in _callbacks, str(id))
+            var args = _split_args(match[2])
+            # print("args ", args)
+            return _callbacks[id].callv(args)
+
+
+
+func run(lines: Array):
+    var loop_stack = []
+    var line_index = 0
+    var skip_depth = 0
+    var code_list = _get_commands_from_sources(lines)
+    var ignore_line = [Keyword.ENDIF]
+    
+    while line_index < lines.size():
+        # print(line_index)
+        var line = lines[line_index]
+        var args = line
+        args = _split_args(args)
+        var code: String = args.pop_front()
+
+        if skip_depth > 0:
+            if code.begins_with(Keyword.IF):
+                skip_depth += 1
+            elif code.begins_with(Keyword.ENDIF):
+                skip_depth -= 1
+            line_index += 1
+            continue
+
+        if code.begins_with(Keyword.IF):
+            if !_check_if_conditions(line):
+                skip_depth = 1
+        elif code.begins_with(Keyword.EXIT):
             break
         
-        var line = _split_args(lines[line_index])
-        print(line[0])
+        elif code in ignore_line:
+            pass
+
+        else:
+            assert(code in _callbacks, str(code))
+            await _callbacks[code].callv([args, code_list.slice(line_index + 1)])
+        
+        line_index += 1
